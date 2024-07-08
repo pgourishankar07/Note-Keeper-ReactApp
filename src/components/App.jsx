1fa94a9c551483dfe908857c49d2659109268759
Gouri Shankar <gourishankar@Gouris-MacBook-Air.local>
2024-07-08 09:32:53 +0530

    added animated backgrond

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,127 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
 import Create from "./Create";
 
-// function noteCard(i) {
-//   return <Note key={i.key} title={i.title} content={i.content} />;
-// }
+import styled, { keyframes } from "styled-components";
+
+// Define the animation keyframes
+const fade = keyframes`
+  from {
+    background: #f1c40f;
+  }
+  to {
+    background: #d35400;
+  }
+`;
+
+// Styled-component for the animated rows
+const Row = styled.div`
+  background: #f1c40f;
+  height: 10vh;
+  width: 100%;
+  position: absolute;
+  animation: ${fade} 5s infinite alternate;
+  &:nth-child(1) {
+    top: 0;
+  }
+  &:nth-child(2) {
+    top: 10vh;
+    animation-delay: 0.5s;
+  }
+  &:nth-child(3) {
+    top: 20vh;
+    animation-delay: 1s;
+  }
+  &:nth-child(4) {
+    top: 30vh;
+    animation-delay: 1.5s;
+  }
+  &:nth-child(5) {
+    top: 40vh;
+    animation-delay: 2s;
+  }
+  &:nth-child(6) {
+    top: 50vh;
+    animation-delay: 2.5s;
+  }
+  &:nth-child(7) {
+    top: 60vh;
+    animation-delay: 3s;
+  }
+  &:nth-child(8) {
+    top: 70vh;
+    animation-delay: 3.5s;
+  }
+  &:nth-child(9) {
+    top: 80vh;
+    animation-delay: 4s;
+  }
+  &:nth-child(10) {
+    top: 90vh;
+    animation-delay: 4.5s;
+  }
+`;
+
+// Styled-component for the rows container
+const RowsContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: -1; /* Behind the main content */
+`;
+
+// Styled-component for the main content container
+const ContentContainer = styled.div`
+  position: relative;
+  z-index: 1; /* On top of the animated rows */
+`;
 
 function App() {
-  let [dnotes, setDnotes] = React.useState([...notes]);
+  const [dnotes, setDnotes] = React.useState([...notes]);
 
-  function addNote(i) {
-    dnotes.push(i);
-    setDnotes([...dnotes]);
+  function addNote(note) {
+    setDnotes((prevNotes) => [...prevNotes, note]);
   }
 
   function deleteNote(id) {
-    setDnotes((preVal) => {
-      return preVal.filter((note, index) => {
-        return index !== id;
-      });
-    });
+    setDnotes((prevNotes) => prevNotes.filter((note, index) => index !== id));
   }
 
   return (
     <div>
-      <Header />
-      <Create onAdd={addNote} />
+      {/* Animation rows container */}
+      <RowsContainer>
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+      </RowsContainer>
 
-      {dnotes.map((noteCard, index) => {
-        return (
+      {/* Main content container */}
+      <ContentContainer>
+        <Header />
+        <Create onAdd={addNote} />
+        {dnotes.map((note, index) => (
           <Note
             key={index}
             id={index}
-            title={noteCard.title}
-            content={noteCard.content}
+            title={note.title}
+            content={note.content}
             onDelete={deleteNote}
           />
-        );
-      })}
+        ))}
+      </ContentContainer>
       <Footer />
     </div>
   );
